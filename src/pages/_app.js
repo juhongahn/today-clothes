a4a4eafcf6437f321339bfa7b3fdbe9da8bf681d
Juhong An <juhong.ahn.dev@gmail.com>
2023-03-24 21:31:54 +0900

    feat: 회원가입, 로그인 페이지 구현

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import Container from '@mui/material/Container';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container fixed maxWidth="xs">
+      <Component {...pageProps} />
+    </Container>
+  )
 }
