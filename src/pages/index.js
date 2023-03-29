f876429c5cc59c27a9f3a6cee11ed70cbfcf6969
Juhong An <juhong.ahn.dev@gmail.com>
2023-03-29 16:10:05 +0900

    feat: 주소에 따른 날씨 정보 가져오기 기능 구현 중

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,30 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from 'next/image';
 import Grid from '@mui/material/Grid';
-
+import Button from '@mui/material/Button'
+import fetchWeather from "@/lib/fetchWeather";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const { data: session, status } = useSession();
+
+  function showTodayCloth() {
+    fetchWeather();
+  }
+
   return (
     <div>
       <Grid container spacing={2}>
+        <Grid item xs={12} md={12}>
+          <Button
+            variant="contained"
+            onClick={() => {
+              showTodayCloth();
+            }}
+          >
+            오늘의 옷
+          </Button>
+        </Grid>
         <Grid item xs={6}>
           <Link href="/my-closet">옷장</Link>
         </Grid>
