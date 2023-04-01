1ab00993eb5dd5e084af8869d24d384797ef6ad8
juhongahn <juhong.ahn.dev@gmail.com>
2023-04-01 18:34:26 +0900

    feat: GPT를 사용해서 날씨에 따른 옷차림 추천 로직 구현

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,10 @@ const url = "http://localhost:3000/api/weather";
 
 function makeScript(weatherData) {
   const { result } = weatherData;
-  const script = `계절은 봄이고, 최대 기온은 ${result.TMX}도, 최저 기온은 ${result.TMN}도 입니다.
-   그리고 하루 중 강수 확률은 ${result.POP}% 입니다. 이 날씨에 적합한 옷차림을 추천 해주세요.
+  const script = `오늘 하루 최고 기온은 ${result.TMX}도, 최저 기온은 ${result.TMN}도야. 평균 풍속은 ${result.WSD}이고 하루 중
+  강수 확률은 ${result.POP}%야. 그리고 평균 적성량은 ${result.SNO}야. 
+   그리고 하루 중 강수 확률은 ${result.POP}% 입니다. 이 날씨에 맞게 하루 동안 입을 옷차림을 추천해줘.
+   '트렌치 코트', '울 코트' 처럼 옷의 종류로 설명해줬으면 좋겠고, 날씨에 어울리는 옷의 소재 또한 설명해줬으면 좋겠어.
   `;
   return script;
 }
