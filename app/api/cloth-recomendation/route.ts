720c1ca418398cd4bae8e7032d39d430c5798478
juhongahn <juhong.ahn.dev@gmail.com>
2023-08-13 20:42:15 +0900

    feat: implement cloth recommentdation feature using open ai api

---
+++ app/api/cloth-recomendation/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { OpenAIStream, OpenAIStreamPayload } from "../../_lib/openaiStream";
+import createPrompts, { createPromptDummy } from "../../_lib/createPrompts";
+
+export async function POST(req: NextRequest) {
+  const {inputPrompt} = await req.json();
+  const prompts = createPrompts(inputPrompt);
+  const messages = createPromptDummy();
+  messages.push({
+    role: "user",
+    content: prompts,
+  });
+  
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: messages,
+    temperature: 0.25,
+    presence_penalty: 0,
+    top_p: 1,
+    frequency_penalty: 0,
+    stream: true,
+    max_tokens: 600,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+}
