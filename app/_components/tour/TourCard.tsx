15782ebe40cb9c0f0d5b7cc3adb21e6aa253fa1f
juhongahn <juhong.ahn.dev@gmail.com>
2023-08-27 18:24:38 +0900

    refactor: 이미지 컴포넌트 size 프롭 변경

--- app/_components/tour/TourCard.tsx
+++ app/_components/tour/TourCard.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import styles from "./TourCard.module.css";
 
 interface TourCardProps {
-  key: number;
   tour: {
     title: string;
     location: string;
@@ -17,10 +16,9 @@ interface TourCardProps {
   }) => void;
 }
 
-const TourCard = ({ key, tour, cardSelectHandler }: TourCardProps) => {
+const TourCard = ({ tour, cardSelectHandler }: TourCardProps) => {
   return (
     <div
-      key={key}
       className={styles.card}
       onClick={cardSelectHandler.bind(null, tour)}
     >
@@ -28,10 +26,10 @@ const TourCard = ({ key, tour, cardSelectHandler }: TourCardProps) => {
         <Image
           className={styles.image}
           src={tour.image}
-          fill
-          sizes="100%"
           alt="여행지 이미지"
           priority
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
       <div className={styles.des}>
