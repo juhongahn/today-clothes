ef1f16d5b401c2f1394328a861049885bcebcd9c
juhongahn <juhong.ahn.dev@gmail.com>
2023-08-28 15:16:43 +0900

    test: 에러페이지 삭제후 배포 테스트 진행

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config, { isServer }) => {
-    // Production 환경에서만 Source Maps를 활성화합니다.
-    if (!isServer) {
-      config.devtool = 'source-map';
-    }
-
-    return config;
-  },
   experimental: {
     appDir: true,
     optimizeCss: true
