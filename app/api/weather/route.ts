73a6285f1e5c9cfe82b182e881a9014e5c22606c
juhongahn <juhong.ahn.dev@gmail.com>
2023-08-29 04:25:20 +0900

    test: console.error(t.value) undefined 에러 원인 파악

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -183,9 +183,8 @@ const convertToUnixTime = (fcstDate: string, fcstTime: string) => {
 
   const date = new Date(year, month, day, hours, minutes);
   const unixTime = date.getTime();
-  // 시간대 보정 (예시: 대한민국의 경우 UTC+9)
-  const timeZoneOffset = 9 * 60 * 60 * 1000; // 밀리초 단위
-  const correctedUnixTime = unixTime + timeZoneOffset;
+  const timeZoneOffset = 9 * 60 * 60 * 1000;
+  const correctedUnixTime = unixTime - timeZoneOffset;
 
   return correctedUnixTime;
 };
