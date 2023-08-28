e7b93d0611041644a003467ed95c0d4f0928851e
juhongahn <juhong.ahn.dev@gmail.com>
2023-08-28 21:58:26 +0900

    test: 테스트 코드 수정

--- __tests__/xlsx.test.ts
+++ __tests__/xlsx.test.ts
@@ -1,10 +1,16 @@
 import getRegId from "../app/_lib/getRegId";
 import { getRegionCodeByRegionName } from "../app/_helpers/constants/midtermForcastRegionCode";
 
+jest.setTimeout(100000);
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
 test("지역 코드 엑셀 파일에서, 시에 매칭되는 지역 코드를 반환하는 테스트", () => {
   expect(getRegId("부산광역시")).toBe("11H20201");
 });
 
 test("시 이름으로 지역 코드 찾는 메서드 테스트", () => {
   expect(getRegionCodeByRegionName("충청북도")).toBe("11C10000");
-})
+});
