ac116ca81ab062f61f498ec30a83d806e5bbd4a1
juhongahn <juhong.ahn.dev@gmail.com>
2023-08-28 20:48:49 +0900

    test: 에러 발생 이전으로  구성 초기화

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const nextConfig = {
     appDir: true,
     optimizeCss: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
