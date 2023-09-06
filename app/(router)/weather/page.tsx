42747f0f76ef0c58c923ba165741823d45f9c5c0
juhongahn <juhong.ahn.dev@gmail.com>
2023-09-06 23:28:14 +0900

    refactor: 안쓰는 TourWrapper 컴포넌트 제거

--- app/(router)/weather/page.tsx
+++ app/(router)/weather/page.tsx
@@ -3,10 +3,10 @@
 import { Provider } from "react-redux";
 import { store } from "../../store";
 import WeatherWrapper from "../../_components/WeatherWrapper";
-import TourWrapper from "../../_components/tour/TourWrapper";
 import SearchSection from "../../_components/searchWeather/SearchSection";
 import styles from "./WeatherPage.module.css";
 import NavBar from "../../_components/navbar/NavBar";
+import Tour from "../../_components/tour/Tour";
 
 const Page = () => {
   return (
@@ -21,7 +21,7 @@ const Page = () => {
             <SearchSection />
           </div>
           <div className={styles.tour}>
-            <TourWrapper />
+            <Tour />
           </div>
         </div>
       </Provider>
