008c2ebaaefd244c7a890c3cb2565170f772235d
juhongahn <juhong.ahn.dev@gmail.com>
2023-09-28 00:41:12 +0900

    에러 로그 제거

--- app/_helpers/error-class/HttpError.ts
+++ app/_helpers/error-class/HttpError.ts
@@ -1,6 +1,8 @@
+import { AxiosResponse } from "axios";
+
 export class HttpError extends Error {
-  response: Response;
-  constructor(message: string, response: Response) {
+  response: Response | AxiosResponse;
+  constructor(message: string, response: Response | AxiosResponse) {
     super(message);
     this.name = "HttpError";
     this.response = response;
